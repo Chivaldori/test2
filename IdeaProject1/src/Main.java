c346b3f7700759979eaa1444e3c7b57308511a32
Enrico Chivaldori <echivaldori@gmail.com>
2014-10-21 22:50:04 +0300

    hello1 my changes

--- IdeaProject1/src/Main.java
+++ IdeaProject1/src/Main.java
@@ -1,8 +1,8 @@
 public class Main {
     public static void main(String[] args) {
         //my comment
-        System.out.println("Hello!");
-        System.out.println("Hello!");
+        System.out.println("Hello!1");
+        System.out.println("Hello!1");
 
 
     }
