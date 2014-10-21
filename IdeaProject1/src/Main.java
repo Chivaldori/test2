50963c24d314fa83700445edaf25aa38f18e5519
Viacheslav Goroveckiy <vgoroveckiy@gmail.com>
2014-10-21 22:47:46 +0300

    commit

--- IdeaProject1/src/Main.java
+++ IdeaProject1/src/Main.java
@@ -2,7 +2,7 @@ public class Main {
     public static void main(String[] args) {
         //my comment
         System.out.println("Hello!");
-        System.out.println("Hello!");
+        System.out.println("Hello2!");
 
 
     }
